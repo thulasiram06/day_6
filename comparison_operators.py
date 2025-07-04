f49a9df06f1272c5e10466259c7257f3b16357a2
thulasiram06 <pemmasanithulasiram569@gmail.com>
2025-07-04 17:39:58 +0530

    comparision operators

---
+++ comparison_operators.py
@@ -0,0 +1,11 @@
+#comparision operators(<,>,=<,=>,==,!=,)
+pen=45
+pencil=36
+print(type(pen))
+print(type(pencil))
+print(pen<pencil)
+print(pen>pencil)
+print(pen<=pencil)
+print(pen>=pencil)
+print(pen==pencil)
+print(pen!=pencil)
